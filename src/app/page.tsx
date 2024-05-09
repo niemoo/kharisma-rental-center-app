a3648f37a40abb530d0cab854f9fd8e6ff41da3e
Izzan Abdul <izzanabdul123@gmail.com>
2024-05-09 23:24:03 +0700

    feat: fetch data API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 import CarsCard from '@/components/CarsCard';
-import agyaManual1 from '../../public/agya-manual-1.jpg';
-import agyaManual2 from '../../public/agya-manual-2.jpg';
-import avanzaVeloz1 from '../../public/avanza-veloz-1.jpg';
-import avanzaVeloz2 from '../../public/avanza-veloz-2.jpg';
-import pickup1 from '../../public/pickup-1.jpg';
-import pickup2 from '../../public/pickup-2.jpg';
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('http://localhost:3001/cars');
+  return res.json();
+}
+
+export default async function Home() {
+  const carsData = await getData();
+  console.log(carsData.data[0]);
   return (
     <main className="max-w-screen-xl mx-auto md:p-0 md:pt-5 p-5">
       <div className="grid md:grid-cols-3 gap-5">
-        <CarsCard foto={agyaManual1} />
-        <CarsCard foto={agyaManual2} />
-        <CarsCard foto={avanzaVeloz1} />
-        <CarsCard foto={avanzaVeloz2} />
-        <CarsCard foto={pickup1} />
-        <CarsCard foto={pickup2} />
+        {carsData.data.map((data: any) => (
+          <CarsCard
+            key={data.id}
+            carId={data.id}
+            carName={data.name}
+            // carImage={`localhost:3001/${data.image}`} // Fixed URL format
+            carCategory={data.category_id}
+            carYear={data.year}
+            carCapacity={data.capacity}
+            carTransmission={data.transmission}
+            carPrice_12={data.price_12}
+            carPrice_24={data.price_24}
+          />
+        ))}
       </div>
     </main>
   );
--- src/components/CarsCard/index.tsx
+++ src/components/CarsCard/index.tsx
@@ -5,41 +5,55 @@ import { TbManualGearbox } from 'react-icons/tb';
 import { Tb12Hours } from 'react-icons/tb';
 import { Tb24Hours } from 'react-icons/tb';
 
-export default function CarsCard({ foto }: { foto: any }) {
+interface CarsCardProps {
+  carId: number;
+  carName: string;
+  // carImage: any;
+  carCategory: number;
+  carYear: string;
+  carCapacity: number;
+  carTransmission: string;
+  carPrice_12: number;
+  carPrice_24: number;
+}
+
+export default function CarsCard({ carId, carName, carCategory, carYear, carCapacity, carTransmission, carPrice_12, carPrice_24 }: CarsCardProps) {
   return (
     <div className="w-full border border-black rounded-lg shadow-lg py-2 px-3 bg-white">
-      <Image src={foto} alt="" className="rounded w-full mb-5" />
-      <h3>Toyota Agya</h3>
-      <h4 className="text-sm text-zinc-500">City Car | 2020</h4>
+      {/* <Image src={carImage} alt="" width={200} height={200} className="rounded w-full mb-5" /> */}
+      <h3>{carName}</h3>
+      <h4 className="text-sm text-zinc-500">
+        {carCategory} | {carYear}
+      </h4>
       <hr className="my-3" />
 
       <div className="mb-5">
         <div className="flex justify-between">
           <div className="flex items-center gap-3">
             <FaUsers className="text-3xl" />
-            <h4>4 Orang</h4>
+            <h4>{carCapacity} Orang</h4>
           </div>
 
           <div className="flex items-center gap-3">
             <Tb12Hours className="text-3xl" />
-            <h4>Rp 220.000</h4>
+            <h4>Rp {carPrice_12}</h4>
           </div>
         </div>
 
         <div className="flex justify-between">
           <div className="flex items-center gap-3">
             <TbManualGearbox className="text-3xl" />
-            <h4>Manual</h4>
+            <h4>{carTransmission}</h4>
           </div>
 
           <div className="flex items-center gap-3">
             <Tb24Hours className="text-3xl" />
-            <h4>Rp 440.000</h4>
+            <h4>Rp {carPrice_24}</h4>
           </div>
         </div>
       </div>
 
-      <Link href={`/mobil/1`}>
+      <Link href={`/mobil/${carId}`}>
         <button className="bg-green-500 hover:bg-green-700 text-white text-center w-full p-2 rounded-lg">Pesan</button>
       </Link>
     </div>
